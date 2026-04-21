1740fcf51872411c5fc97e54b2321e6954031bb9
Sabrina Dignard <sabrina.di97@hotmail.fr>
2026-04-20 21:27:36 -0400

    Fait la modale et tentative de correction du bouton dans conseils

--- js/scripts.js
+++ js/scripts.js
@@ -9,11 +9,24 @@ for (let i = 0; i < boutons.length; i++) {
     const bouton = boutons[i];
 
     bouton.addEventListener('click', ouvrirModale);
-    console.log(boutons);
 }
 
 // Fonctions
 
 function ouvrirModale() {
-    console.log(modale);
+    const text = document.createElement('p');
+    text.innerText = 'Fin';
+    text.classList.add('fenetre');
+
+    modale.appendChild(text);
+    modale.classList.add('visible');
+
+    // Fermer modale
+    
+    text.addEventListener('click', fermerModale);
+}
+
+function fermerModale(event) {
+    modale.removeChild(event.currentTarget);
+    modale.classList.remove('visible');
 }
